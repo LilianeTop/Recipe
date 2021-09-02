b18bc4dcac6412b9b5436ca57d580dd79666aba8
Liliane Top <Liliane.top@procura.nl>
2021-09-02 17:22:00 +0200

    checking if I can push from here to my private git hub account

--- Recipe/src/main/java/gure/springframework/recipe/RecipeApplication.java
+++ Recipe/src/main/java/gure/springframework/recipe/RecipeApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecipeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecipeApplication.class, args);
-    }
 
+        SpringApplication.run(RecipeApplication.class, args);
+        System.out.println("Checking if I can push");
+    }
 }
